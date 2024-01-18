40e3bc7b6518fded18762779331111dd78b2074f
R3D1NM <r3dinm@gmail.com>
2024-01-18 21:31:04 +0900

    [feat] login modal

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from './page.module.css'
-import Logo from '../../public/logo.jpg'
+import styles from '@/app/page.module.css'
+import Logo from '../../../public/logo.jpg'
 
 
 export default function Home() {
@@ -11,7 +11,7 @@ export default function Home() {
         <Image src={Logo} alt='logo'/>
       </div>
       <div className={styles.right}>
-        <h1>What is goining on now</h1>
+        <h1>What's going on now</h1>
         <h2>Sign up now</h2>
         <Link href="/i/flow/signup" className={styles.signup}>Create Account</Link>
         <h3>Already signed?</h3>
